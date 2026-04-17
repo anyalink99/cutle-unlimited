2752618e878123f10d78f1af579b2c6df6fa2f2a
anyalink99 <anyalink99@gmail.com>
2026-04-17 04:28:02 +0300

    Remove backdrop ring and double shape area

--- js/core/constants.js
+++ js/core/constants.js
@@ -2,11 +2,11 @@ const SVG_NS = 'http://www.w3.org/2000/svg';
 
 const CX = 200;
 const CY = 200;
-const MAX_R = 168;
-const BASE_R = 118;
+const MAX_R = 196;
+const BASE_R = 167;
 const TAU = Math.PI * 2;
 
-const TARGET_AREA = 30000;
+const TARGET_AREA = 60000;
 
 const MOVE_THRESHOLD = 6;
 const STATS_KEY = 'geometric.games.stats.v1';
